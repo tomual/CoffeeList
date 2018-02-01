b696779dd869cc5c0ced50841090b3f7831d74b3
tomual <tomual@users.noreply.github.com>
2018-01-31 21:59:44 -0600

    Checkbox in ListView

--- src/sample/TaskList.java
+++ src/sample/TaskList.java
@@ -1,6 +1,8 @@
 package sample;
 
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXCheckBox;
+import com.jfoenix.controls.JFXListCell;
 import com.jfoenix.controls.JFXListView;
 
 import java.io.IOException;
@@ -14,7 +16,13 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.layout.AnchorPane;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Paint;
 
 public class TaskList {
 
@@ -59,6 +67,7 @@ public class TaskList {
             observableList = database.getTasks(user.getUserId());
         }
         listView.setItems(observableList);
+        listView.setCellFactory(param -> new JFXCell());
         assert listView != null : "fx:id=\"listView\" was not injected: check your FXML file 'taskList.fxml'.";
         assert addButton != null : "fx:id=\"addButton\" was not injected: check your FXML file 'taskList.fxml'.";
 
@@ -67,5 +76,32 @@ public class TaskList {
     public void setUser(User user) {
         this.user = user;
         System.out.println("Hello, " + user.getUsername() + "!");
+    }
+
+    static class JFXCell extends JFXListCell<String> {
+        HBox hBox = new HBox();
+        JFXCheckBox checkBox = new JFXCheckBox();
+        Label labelLabel = new Label();
+
+        Pane pane = new Pane();
+
+        public JFXCell() {
+            super();
+
+            hBox.getChildren().addAll(checkBox, labelLabel);
+            hBox.setHgrow(pane, Priority.ALWAYS);
+            hBox.setPadding(new Insets(5, 2.5, 5, 2.5));
+            checkBox.setCheckedColor(Paint.valueOf("#29434e"));
+        }
+
+        public void updateItem(String label, boolean checked) {
+            super.updateItem(label, checked);
+            setText(null);
+
+            if(label != null && !checked) {
+                labelLabel.setText(label);
+                setGraphic(hBox);
+            }
+        }
     }
 }
